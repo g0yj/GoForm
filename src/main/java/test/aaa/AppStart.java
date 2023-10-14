08c4c55b877f39cd6bdb7243b54c495941b0912a
gyj <gyj@gmail.com>
2023-10-14 11:41:44 +0900

    Initial commit

---
+++ src/main/java/test/aaa/AppStart.java
@@ -0,0 +1,9 @@
+package test.aaa;
+
+public class AppStart {
+    public static void main(String[] args) {
+        System.out.println("sss");
+        System.out.printf("ㅋㅋㅋㅋㅋㅋ");
+        System.out.println("한글 왜 안나오냐 ,, ");
+    }
+}
